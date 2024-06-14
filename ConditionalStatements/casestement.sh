0ba964eb8d510467e4b21ef24baf0bce14761304
Bhratesh Dhangar <bhrateshd@gmail.com>
2024-06-14 11:27:28 +0530

    ifelse

---
+++ ConditionalStatements/casestement.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo "enter a fruit name"
+read fruit
+#fruit="apple"
+
+case $fruit in
+    "apple")
+        echo "This is a red fruit."
+        ;;
+    "banana")
+        echo "This is a yellow fruit."
+        ;;
+    "orange")
+        echo "This is an orange fruit."
+        ;;
+    *)
+        echo "Unknown fruit."
+        ;;
+esac
