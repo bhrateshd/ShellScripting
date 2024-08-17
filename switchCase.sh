f016e97336202eb232acf0a69a05d5dbd0d077dd
purushottamgutthe <purushottamgutthe6767@gmail.com>
2024-08-17 13:41:32 +0530

    Swtich case program added, completed

--- switchCase.sh
+++ switchCase.sh
@@ -2,3 +2,17 @@
 
 read -p "Enter any single digit number": choice
 
+case $choice in
+	1) echo "You entered one"
+		;;
+	3) echo "You entered three"
+		;;
+	4) echo "your input is 4"
+		;;
+	8) echo "your input is 8"
+		;;
+	9) echo "Now this is 9th"
+		;;
+	*) echo "your input is not matching any case"
+		;;
+esac
