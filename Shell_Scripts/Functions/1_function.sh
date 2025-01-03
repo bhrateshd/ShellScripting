85c17d2eea00e361406ab20fa7b6ebf381e99f64
Bhratesh <bhrateshd@gmail.com>
2025-01-03 15:51:20 +0530

    adding Functions

---
+++ Shell_Scripts/Functions/1_function.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+<< COMMENT
+======================
+Functions / Methods
+======================
+
+=> Functions are used to perform some action / task.
+
+=> Using functions we can divide big task into multiple small tasks.
+
+=> Using functions we can divide our work logically
+
+=> Functions are re-usable
+
+-------
+syntax
+-------
+COMMENT
+
+# create function
+funName() {
+	echo "this is a test comment" 
+}
+
+# call function for execution
+funName
+
+#====================== shell script with Function ===============
+
+#! /bin/bash
+
+welcome(){
+     echo "welcome to ashokit"
+     echo "welcome to devops"
+     echo "welcome to aws"
+}
+
+welcome
+#-------------------------------------------------
+
