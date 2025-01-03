bc9cbd46a878bb6d2a88a28e242fe64fb01936c6
Bhratesh <bhrateshd@gmail.com>
2025-01-03 17:20:41 +0530

    added new option to hide pass -s

--- Shell_Scripts/Variables/2_read_display.sh
+++ Shell_Scripts/Variables/2_read_display.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-echo "Enter a number:"
-read num
-echo “The number is: $num”
+#echo "Enter a number:"
+#read num
+#echo “The number is: $num”
+
+#=======================================
+
+echo -n "Enter your name: "
+read name
+echo -n "Enter your location: "
+read loc
+
+#Below option -s is used to hide the password at the time of typing
+read -s -p 'Enter your password:' pass
+
+echo -e "\nYour Name is = $name\nYour Location is =$loc\nYour Password = $pass "
