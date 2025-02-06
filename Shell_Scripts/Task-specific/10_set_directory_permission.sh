183876ae92b1de317627fadeaca018e8389d2332
Bhratesh <bhrateshd@gmail.com>
2025-02-07 01:00:42 +0530

    adding comment

--- Shell_Scripts/Task-specific/10_set_directory_permission.sh
+++ Shell_Scripts/Task-specific/10_set_directory_permission.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 read -p "Enter the directory name: " dir
-
+#adding the comment for just testing
 if [ -d $dir ]; then
 chmod u+rwx $dir
 echo "Directory permissions have been updated!"
 else
 echo "Error! The directory $dir does not exist."
 fi
+
